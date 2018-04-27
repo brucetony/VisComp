24edf10f52f59c5fdc88d113be3c54bcc4fd446a
brucetony <bschultz013@gmail.com>
2018-04-27 20:39:19 +0200

    Fix PEP errors, scat_matrix() now prints highest DSC pair and value, histograms have border now

--- vclsi-2-Schultz-Ibarra.py
+++ vclsi-2-Schultz-Ibarra.py
@@ -25,10 +25,10 @@ def DSC(point_list):
     :param point_list: A list of points in tuple format
     :return: Numerical computation of the Distance consistency for all points provided
     '''
-    #TODO check if this is right
+    # TODO check if this is right
     # First computer center points
     centers = cluster_center(point_list)
-    #Create empty lists to store distances of the difference between each point and each centroid
+    # Create empty lists to store distances of the difference between each point and each centroid
     same_cluster_distances = []
     diff_cluster_distances = []
     for i in range(len(point_list)):
@@ -55,7 +55,7 @@ def find_missing_values(pds_array):
     for i in pds_array.columns.values:
         if pds_array[i].isnull().values.any():
             null_vectors.append(i) #Add to list of vectors with missing values
-            print("{} contains null values".format(i))
+            print("{} contains null values...fixing".format(i))
     if not null_vectors:
         print("No null values were found")
     return null_vectors
@@ -68,19 +68,19 @@ def interpolate_values(pandasDF):
     '''
     null_vectors = find_missing_values(pandasDF)
     for vector in null_vectors:
-        #Slice empty vectors
+        # Slice empty vectors
         benign = pandasDF[pandasDF['class'] == 2]
         malig = pandasDF[pandasDF['class'] == 4]
 
-        #Drop na and find average for each class, rounded to nearest integer
+        # Drop na and find average for each class, rounded to nearest integer
         benign_av = int(round(np.mean(benign[vector].dropna())))
         malig_av = int(round(np.mean(malig[vector].dropna())))
 
-        #Fill null with average values
+        # Fill null with average values
         benign = benign.fillna(benign_av)
         malig = malig.fillna(malig_av)
 
-        #Concatenate pandas frames
+        # Concatenate pandas frames
         pandasDF_no_null = pd.concat([benign, malig])
     return pandasDF_no_null
 
@@ -94,55 +94,54 @@ def scat_matrix():
         columns = columns[1:]
     bc_data = pd.read_excel('breast-cancer-wisconsin.xlsx')
 
-    #Interpolate missing data
+    # Interpolate missing data
     bc_data = interpolate_values(bc_data)
 
-    #Generate figure for plots to be plotted in
+    # Generate figure for plots to be plotted in
     size = len(columns)
     fig, subs = plt.subplots(size, size, figsize=(15, 15))
 
-    #Used for tracking plot placement
+    # Used for tracking plot placement
     i = 0
     j = 0
 
-    #Create colors list
+    # Create colors list
     colors = ["blue" if row is 2 else "red" for row in list(bc_data['class'])]
 
-    #Separate Data
+    # Separate Data
     benign_data = bc_data[bc_data['class'] == 2]
     malig_data = bc_data[bc_data['class'] == 4]
 
-    #Iterate through column names and generate plots
+    # Collect DSC values
+    DSC_values = []
+
+    # Iterate through column names and generate plots
     for axis1 in columns:
         for axis2 in columns:
             if axis1 == axis2:
-                #Make diagonal plots different
+                # Make diagonal plots different
                 benign_sd = list(benign_data[axis1])
                 malig_sd = list(malig_data[axis1])
 
-                #Generate histogram, density parameter means normalized
-                subs[i, j].hist(benign_sd, color = 'blue', label = 'benign', histtype='bar')
-                subs[i, j].hist(malig_sd, color='red', label='malig', histtype='bar')
+                # Generate histogram, density parameter means normalized
+                subs[i, j].hist(benign_sd, color = 'blue', label = 'benign', histtype='bar', edgecolor='black')
+                subs[i, j].hist(malig_sd, color='red', label='malig', histtype='bar', edgecolor='black')
                 subs[i, j].set_title(axis1)
                 #axes[i, j].xlabel(axis1)
                 #axes[i, j].ylabel('Frequency')
-
-                #TODO put border around bar
-                #TODO flip so diagonal goes the other way?
-                #TODO verify why some graphs look like >1 when normalized
             else:
-                #Generate sizes by counting number of times a point occurs
+                # Generate sizes by counting number of times a point occurs
                 datapoints = list(zip(bc_data[axis1], bc_data[axis2]))
                 sizes = [datapoints.count(point)*2 for point in datapoints]
 
-                #Create scatter plot at position i,j using created sizes/colors, alpha used for overlapping points
+                # Create scatter plot at position i,j using created sizes/colors, alpha used for overlapping points
                 subs[i, j].scatter(bc_data[axis1], bc_data[axis2], alpha=0.25, c=colors, s=sizes)
 
-                #Setup to calculate DSC
+                # Setup to calculate DSC
                 benign_scatter = list(zip(benign_data[axis1], benign_data[axis2]))
                 malig_scatter = list(zip(malig_data[axis1], malig_data[axis2]))
                 all_points = [benign_scatter, malig_scatter]
-                print('DSC of {} and {} = {}'.format(axis1, axis2, DSC(all_points)))
+                DSC_values.append(('{} and {}'.format(axis1, axis2), DSC(all_points)))
 
             # Increase counter to change position
             if i < size-1:
@@ -156,5 +155,10 @@ def scat_matrix():
     fig.suptitle('Malignant and Benign Tumor Values', x=0.5, y=1.0)
     #fig.savefig('Scatter Plot.png')
     fig.show()
+
+    # Determine max DSC value and return highest one
+    DSC_values.sort(key=lambda x: x[1])
+    print("The highest distance consistancy is {} and found between {}".format(DSC_values[-1][1], DSC_values[-1][0]))
+
 
 scat_matrix()
